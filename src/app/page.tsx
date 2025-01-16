c5bb1c947513f709d46e7661a2beee0154ec7401
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-16 13:45:59 +0330

    styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
-    <div className="">
-      <p className="text-5xl ">Hello World</p>
-      <Button>test</Button>
-    </div>
+    <div className="mt-20">main content</div>
   );
 }
--- src/components/sections/top-nav/index.tsx
+++ src/components/sections/top-nav/index.tsx
@@ -1,9 +1,19 @@
 const TopNav = () => {
   return (
     <div>
-      <p>Top Nav</p>
+      <TopSection />
     </div>
   );
 };
 
 export default TopNav;
+
+const TopSection = () => {
+  return (
+    <div className="flex justify-between content-paddign">
+      <div>Logo and Name</div>
+      <div>Maybe Search</div>
+      <div>Cart and User Profile</div>
+    </div>
+  );
+};
