63c501df69db605763f78fece9b1869d79e2b819
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-16 17:13:35 +0330

    BottomSection

--- src/components/sections/top-nav/bottom-section.tsx
+++ src/components/sections/top-nav/bottom-section.tsx
@@ -1,5 +1,18 @@
+import { ReactNode } from "react";
+
 const BottomSection = () => {
-  return <div>BottomSection</div>;
+  return (
+    <div className="mt-2 flex font-semibold text-sm text-primary/80 pb-2">
+      <CategoryItemWraper>All Products</CategoryItemWraper>
+      <CategoryItemWraper>Electronics</CategoryItemWraper>
+      <CategoryItemWraper>Jewelery</CategoryItemWraper>
+      <CategoryItemWraper>Mens clothing</CategoryItemWraper>
+    </div>
+  );
 };
 
 export default BottomSection;
+
+const CategoryItemWraper = ({ children }: { children: ReactNode }) => {
+  return <p className="hover:border-b-2 border-red cursor-pointer pr-6">{children}</p>;
+};
--- src/components/sections/top-nav/index.tsx
+++ src/components/sections/top-nav/index.tsx
@@ -3,7 +3,7 @@ import TopSection from "./top-section";
 
 const TopNav = () => {
   return (
-    <div className="border-b shadow content-paddign-x content-paddign-y">
+    <div className="border-b shadow content-paddign-x content-paddign-y !pb-0">
       <TopSection />
       <BottomSection />
     </div>
