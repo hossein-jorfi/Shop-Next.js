c7fe96adc82e3f35f25525346aaca8bcbe848e64
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-16 12:43:55 +0330

    add button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div>
+    <div className="text-center">
       <p className="text-5xl text-center">Hello World</p>
+      <Button>test</Button>
     </div>
   );
 }
