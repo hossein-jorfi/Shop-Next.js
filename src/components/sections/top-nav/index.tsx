fd9b8ec2f9d5e8ca455de928a0b34ce9fa3429d0
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-16 16:53:23 +0330

    BottomSection component

--- src/components/sections/top-nav/index.tsx
+++ src/components/sections/top-nav/index.tsx
@@ -1,9 +1,11 @@
+import BottomSection from "./bottom-section";
 import TopSection from "./top-section";
 
 const TopNav = () => {
   return (
     <div className="border-b shadow content-paddign-x content-paddign-y">
       <TopSection />
+      <BottomSection />
     </div>
   );
 };
