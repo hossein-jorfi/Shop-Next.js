7b4d2c99523c6ff8a3574a77cf4f3166581797ab
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-16 14:51:04 +0330

    assets

--- src/components/sections/top-nav/index.tsx
+++ src/components/sections/top-nav/index.tsx
@@ -1,6 +1,8 @@
+import { GlobeAltIcon } from "@heroicons/react/24/outline";
+
 const TopNav = () => {
   return (
-    <div>
+    <div className="border-b py-5 shadow">
       <TopSection />
     </div>
   );
@@ -11,8 +13,13 @@ export default TopNav;
 const TopSection = () => {
   return (
     <div className="flex justify-between content-paddign">
-      <div>Logo and Name</div>
-      <div>Maybe Search</div>
+      <div className="flex gap-10 items-center">
+        <div className="text-red flex items-center">
+          <GlobeAltIcon width={42} height={42} />
+          <p className="font-bold text-4xl">Shop</p>
+        </div>
+        <div>Maybe Search</div>
+      </div>
       <div>Cart and User Profile</div>
     </div>
   );
