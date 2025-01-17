f3dcc793fd5ed2b111a4e74f5e20e0d33a934b6f
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-17 15:33:36 +0330

    Categories component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Categories from "@/ui/home/categories";
 import { MainSlider } from "@/ui/home/main-slider";
 
 export default function Home() {
   return (
     <div>
       <MainSlider />
+      <Categories />
     </div>
   );
 }
