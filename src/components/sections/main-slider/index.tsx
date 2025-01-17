c5e2c16ce9e0da8e619dd45a9ea66bb5fa64f90f
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-17 15:18:05 +0330

    Carousel autoplay

--- src/components/sections/main-slider/index.tsx
+++ src/components/sections/main-slider/index.tsx
@@ -9,35 +9,34 @@ import {
 } from "@/components/ui/carousel";
 
 export function MainSlider() {
+
   const nextRef = useRef<HTMLButtonElement>(null);
-  const prevRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     setInterval(() => {
       nextRef.current?.click();
-    }, 5000);
+    }, 2000);
   }, []);
+
 
   return (
     <Carousel
       opts={{
         align: "start",
+        loop: true,
       }}
       className="w-full"
     >
       <CarouselContent>
         {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem
-            key={index}
-            className="h-80"
-          >
+          <CarouselItem key={index} className="h-80">
             <div className="h-full border flex justify-center items-center bg-red/50">
               {index + 1}
             </div>
           </CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious ref={prevRef} className="left-5" />
+      <CarouselPrevious className="left-5" />
       <CarouselNext ref={nextRef} className="right-5" />
     </Carousel>
   );
