b1106e0d5d15498630b60c1cdd59ee4d7352cefc
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-17 21:49:20 +0330

    ProductType

---
+++ src/definitions.ts
@@ -0,0 +1,9 @@
+export interface ProductType {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  rating: { rate: number; count: number };
+  title: string;
+}
