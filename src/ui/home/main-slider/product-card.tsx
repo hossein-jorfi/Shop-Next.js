9cd93e4c298fb9c8ef977c2d9c5257523d36666f
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-17 21:23:47 +0330

    styles

--- src/ui/home/main-slider/product-card.tsx
+++ src/ui/home/main-slider/product-card.tsx
@@ -3,15 +3,16 @@ import Image from "next/image";
 
 const ProductCard = () => {
   return (
-    <div className="bg-background h-full rounded-xl">
-      <AspectRatio>
+    <div className="bg-background h-full rounded-xl overflow-hidden flex flex-col justify-between items-center">
+      <AspectRatio className="flex justify-center items-center">
         <Image
           src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
           alt=""
-          width={20}
-          height={20}
+          width={70}
+          height={70}
         />
       </AspectRatio>
+      <p className="px-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut</p>
     </div>
   );
 };
