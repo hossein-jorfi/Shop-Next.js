d35e4914d70b8367748e71c9eb5b15d7f0fe548a
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-17 15:47:45 +0330

    Categories

--- src/ui/home/categories/index.tsx
+++ src/ui/home/categories/index.tsx
@@ -1,5 +1,35 @@
+const ITEMS = [
+  {
+    title: "Electronics",
+    href: "#",
+  },
+  {
+    title: "Jewelery",
+    href: "#",
+  },
+  {
+    title: "Mens clothing",
+    href: "#",
+  },
+  {
+    title: "Women's clothing",
+    href: "#",
+  },
+];
+
 const Categories = () => {
-  return <div>categories</div>;
+  return (
+    <div className="grid grid-cols-2 grid-rows-4 gap-4 mt-5 px-5">
+      {ITEMS.map((item, index) => (
+        <div
+          className="border h-52 flex justify-center items-center rounded-xl text-red"
+          key={index}
+        >
+          {item.title}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Categories;
