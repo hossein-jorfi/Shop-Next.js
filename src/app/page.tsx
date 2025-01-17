87ac4009a4fe1af82d0f0d1e123d6e6a45be55eb
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-17 17:37:35 +0330

    home page comtaoner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ import { MainSlider } from "@/ui/home/main-slider";
 export default function Home() {
   return (
     <div>
-      <div className="">
-        <div className="pattern h-16" /> 
-      </div>
+      <div className="pattern h-16" />
 
-      <MainSlider />
-      <Categories />
+      <div className="custom-container">
+        <MainSlider />
+        <Categories />
+      </div>
     </div>
   );
 }
