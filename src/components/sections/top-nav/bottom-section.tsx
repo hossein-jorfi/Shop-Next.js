4f0f4d2c68fdae234bfd009de691aea8a261ac34
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-17 11:43:07 +0330

    hover logic

--- src/components/sections/top-nav/bottom-section.tsx
+++ src/components/sections/top-nav/bottom-section.tsx
@@ -22,16 +22,24 @@ const ITEMS = [
 ];
 
 const BottomSection = () => {
-  const [hovered, setHovered] = useState(1);
+  const [hovered, setHovered] = useState(-1);
 
   return (
-    <div className="mt-4 flex font-semibold text-sm pb-2">
+    <div className="mt-4 flex font-semibold text-sm pb-2 space-x-4">
       {ITEMS.map((item, index) => (
-        <CategoryItemWraper key={index} title={item.title}>
+        <CategoryItemWraper
+          key={index}
+          title={item.title}
+          setIsHovered={(value) => {
+            if (value) {
+              setHovered(index)
+            } else setHovered(-1)
+          }}
+        >
           {index === hovered && (
             <motion.div
-              className="w-full h-[3px] rounded absolute -bottom-1"
-              style={{ backgroundColor: "red" }}
+              className="w-full h-[2px] rounded absolute -bottom-2 bg-red"
+              transition={{ duration: 0.5 }}
             />
           )}
         </CategoryItemWraper>
@@ -45,12 +53,18 @@ export default BottomSection;
 const CategoryItemWraper = ({
   children,
   title,
+  setIsHovered,
 }: {
-  children: ReactNode;
   title: string;
+  setIsHovered: (value: boolean) => void;
+  children: ReactNode;
 }) => {
   return (
-    <div className="relative">
+    <div
+      className="relative"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <p className="cursor-pointer text-primary/70">{title}</p>
       {children}
     </div>
