f99f27e18643bc4d43596519fb722d1d759eea9b
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-17 21:20:22 +0330

    config

--- src/ui/home/main-slider/product-card.tsx
+++ src/ui/home/main-slider/product-card.tsx
@@ -1,7 +1,17 @@
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import Image from "next/image";
+
 const ProductCard = () => {
   return (
     <div className="bg-background h-full rounded-xl">
-      <p>ProductCard</p>
+      <AspectRatio>
+        <Image
+          src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+          alt=""
+          width={20}
+          height={20}
+        />
+      </AspectRatio>
     </div>
   );
 };
