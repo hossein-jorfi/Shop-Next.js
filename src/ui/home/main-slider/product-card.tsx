2a6c6ea37015ef41ea8369f463de2c53a8fae061
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-18 16:01:21 +0330

    product card styles

--- src/ui/home/main-slider/product-card.tsx
+++ src/ui/home/main-slider/product-card.tsx
@@ -12,9 +12,12 @@ const ProductCard = ({ product }: Props) => {
       <AspectRatio className="flex justify-center items-center">
         <Image src={product.image} alt="product" width={70} height={70} />
       </AspectRatio>
-      <p className="px-1 text-sm font-semibold text-primary/70 overflow-hidden">{product.title}</p>
-      <p className="text-xs text-left w-full px-2 font-bold text">
-        {product.price} <span>USD</span>
+
+      <p className="px-2 h-[44px] text-sm font-semibold text-muted-foreground overflow-hidden">
+        {product.title}
+      </p>
+      <p className="px-2 text-xs text-left w-full font-bold text">
+        {product.price} <span className="text-[11px] font-extrabold text-primary/80">USD</span>
       </p>
     </div>
   );
