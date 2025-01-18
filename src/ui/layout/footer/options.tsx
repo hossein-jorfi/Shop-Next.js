a058c4af189feaa7cfdc392e094b8b4296637dca
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-18 12:41:12 +0330

    Options

--- src/ui/layout/footer/options.tsx
+++ src/ui/layout/footer/options.tsx
@@ -1,5 +1,34 @@
+import guarantee from "@/assets/footer/guarantee.svg";
+import cash_on_delivery from "@/assets/footer/cash-on-delivery.svg";
+import days_return from "@/assets/footer/days-return.svg";
+import express_delivery from "@/assets/footer/express-delivery.svg";
+import support from "@/assets/footer/support.svg";
+
+const ITEMS = [
+  {
+    title: "Guarantee",
+    icon: guarantee,
+  },
+  {
+    title: "7 Days Return",
+    icon: days_return,
+  },
+  {
+    title: "24 hours a day, 7 days a week",
+    icon: support,
+  },
+  {
+    title: "Cash On Delivery",
+    icon: cash_on_delivery,
+  },
+  {
+    title: "Special Delivery",
+    icon: express_delivery,
+  },
+];
+
 const Options = () => {
-  return <div>Options</div>;
+  return <div></div>;
 };
 
 export default Options;
