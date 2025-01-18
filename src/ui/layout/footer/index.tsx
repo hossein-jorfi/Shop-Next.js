5cc1b8c9cd620472227974d7117174b41adf2064
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-18 12:35:44 +0330

    Options componen

--- src/ui/layout/footer/index.tsx
+++ src/ui/layout/footer/index.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { ChevronUp } from "lucide-react";
 import Info from "./info";
+import Options from "./options";
 
 const Footer = () => {
   return (
     <div className="mt-10 py-10 border-t">
-      <div className="custom-container content-paddign-x">
+      <div className="custom-container content-paddign-x flex flex-col gap-12">
         <div className="flex justify-between">
           <Info />
           <Button
@@ -16,6 +17,8 @@ const Footer = () => {
             <ChevronUp className="absolute right-1.5" />
           </Button>
         </div>
+
+        <Options />
       </div>
     </div>
   );
