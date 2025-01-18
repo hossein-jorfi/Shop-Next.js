cab4ca022643d2ae7f22ced9c1c6d8e992b61655
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-18 11:54:04 +0330

    footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import TopNav from "@/ui/layout/top-nav";
+import Footer from "@/ui/layout/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <TopNav />
         <div className="mt-[107px]">{children}</div>
+        <Footer />
       </body>
     </html>
   );
