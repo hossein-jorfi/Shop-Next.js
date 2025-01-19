3968e9e831d40eeb4c0cfbd6c722458550c9dc47
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-19 13:02:38 +0330

    nav items styles

--- src/ui/layout/top-nav/bottom-section.tsx
+++ src/ui/layout/top-nav/bottom-section.tsx
@@ -1,84 +1,41 @@
 "use client";
-import { ReactNode, useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
 
 const ITEMS = [
   {
     title: "All Products",
-    href: "#",
+    href: "/shop",
   },
   {
     title: "Electronics",
-    href: "#",
+    href: "/shop?categories=electronics",
   },
   {
     title: "Jewelery",
-    href: "#",
+    href: "/shop?categories=jewelery",
   },
   {
     title: "Mens clothing",
-    href: "#",
+    href: "/shop?categories=mens",
   },
   {
     title: "Women's clothing",
-    href: "#",
+    href: "/shop?categories=women",
   },
 ];
 
 const BottomSection = () => {
-  const [hovered, setHovered] = useState(-1);
-
   return (
     <div className="mt-4 hidden sm:flex font-semibold text-sm pb-2 space-x-4">
-      <AnimatePresence>
-        {ITEMS.map((item, index) => (
-          <CategoryItemWraper
-            key={index}
-            title={item.title}
-            setIsHovered={(value) => {
-              if (value) {
-                setHovered(index);
-              } else setHovered(-1);
-            }}
-          >
-            {index === hovered && (
-              <motion.div
-                key={index}
-                className="h-[2px] rounded absolute -bottom-2 bg-red"
-                initial={{ width: 0 }}
-                animate={{ width: "100%" }}
-                transition={{ duration: 0.5, ease: "anticipate" }}
-                exit={{
-                  opacity: 0,
-                }}
-              />
-            )}
-          </CategoryItemWraper>
-        ))}
-      </AnimatePresence>
+      {ITEMS.map((item, index) => (
+        <div key={index} className="relative group cursor-pointer">
+          <p className="text-muted-foreground group-hover:text-primary transition-all">
+            {item.title}
+          </p>
+          <div className="h-[2px] rounded absolute -bottom-2 w-0 group-hover:!w-full bg-red transition-all ease-out" />
+        </div>
+      ))}
     </div>
   );
 };
 
 export default BottomSection;
-
-const CategoryItemWraper = ({
-  children,
-  title,
-  setIsHovered,
-}: {
-  title: string;
-  setIsHovered: (value: boolean) => void;
-  children: ReactNode;
-}) => {
-  return (
-    <div
-      className="relative"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <p className="cursor-pointer text-muted-foreground">{title}</p>
-      {children}
-    </div>
-  );
-};
