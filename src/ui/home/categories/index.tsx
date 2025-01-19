d41a6b2623c5b439c592ffde5ad7a361e7c614af
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-19 12:11:25 +0330

    Categories navigation

--- src/ui/home/categories/index.tsx
+++ src/ui/home/categories/index.tsx
@@ -2,26 +2,27 @@ import DevicesSVG from "@/assets/common/devices";
 import JewelerySVG from "@/assets/common/jewelery";
 import MensHoodieSVG from "@/assets/common/mens-hoodie";
 import WomensTshirtSVG from "@/assets/common/womens-t-shirt";
+import Link from "next/link";
 
 const ITEMS = [
   {
     title: "Electronics",
-    href: "#",
+    href: "/shop?categories=electronics",
     icon: <DevicesSVG />,
   },
   {
     title: "Jewelery",
-    href: "#",
+    href: "/shop?categories=jewelery",
     icon: <JewelerySVG />,
   },
   {
     title: "Mens clothing",
-    href: "#",
+    href: "/shop?categories=mens",
     icon: <MensHoodieSVG />,
   },
   {
     title: "Women's clothing",
-    href: "#",
+    href: "/shop?categories=women",
     icon: <WomensTshirtSVG />,
   },
 ];
@@ -30,13 +31,14 @@ const Categories = () => {
   return (
     <div className="grid grid-cols-2 grid-rows-2 gap-4 mt-5 px-5 custom-container md:px-24 lg:px-52">
       {ITEMS.map((item, index) => (
-        <div
+        <Link
+          href={item.href}
           className="border h-52 flex justify-center items-center rounded-xl cursor-pointer hover:border-red/70 hover:text-red font-semibold transition-all flex-col gap-2 overflow-hidden p-10"
           key={index}
         >
           {item.icon}
           <p>{item.title}</p>
-        </div>
+        </Link>
       ))}
     </div>
   );
