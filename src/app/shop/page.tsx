ff0ff80b31e4b057ea73f77c4fa74fcb4f508eb8
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-19 18:26:44 +0330

    grid

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -2,8 +2,13 @@ import ShopBreadcrumb from "@/ui/shop/shop-breadcrumb";
 
 const Shop = () => {
   return (
-    <div className="flex flex-col items-start custom-container content-paddign-x content-paddign-y">
+    <div className="flex flex-col items-start custom-container content-paddign-x content-paddign-y h-full">
       <ShopBreadcrumb />
+
+      <div className="mt-5 flex divide-x border w-full h-full">
+        <div className="p-3 w-80 h-full">Filters</div>
+        <div className="p-3 w-full h-full">Products</div>
+      </div>
     </div>
   );
 };
