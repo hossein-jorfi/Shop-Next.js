44a1a76f911e262c64704907a17b644a18f3ad6a
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-20 07:47:14 +0330

    Filters component

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,3 +1,4 @@
+import Filters from "@/ui/shop/filters";
 import ShopBreadcrumb from "@/ui/shop/shop-breadcrumb";
 
 const Shop = () => {
@@ -5,9 +6,9 @@ const Shop = () => {
     <div className="flex flex-col items-start custom-container content-paddign-x content-paddign-y h-full">
       <ShopBreadcrumb />
 
-      <div className="mt-5 flex divide-x border w-full h-full">
-        <div className="p-3 w-80 h-full">Filters</div>
-        <div className="p-3 w-full h-full">Products</div>
+      <div className="mt-5 flex gap-3 w-full h-full">
+        <Filters />
+        <div className="p-3 w-full border h-full">Products</div>
       </div>
     </div>
   );
---
+++ src/ui/shop/filters.tsx
@@ -0,0 +1,9 @@
+const Filters = () => {
+  return (
+    <div className="p-3 w-80 border rounded-lg h-80">
+      <p>Filters</p>
+    </div>
+  );
+};
+
+export default Filters;
