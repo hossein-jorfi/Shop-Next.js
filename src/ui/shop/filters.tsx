d09d5a27430de6562bff862e0c607141c45b9afe
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-20 14:03:46 +0330

    styles

--- src/ui/shop/filters.tsx
+++ src/ui/shop/filters.tsx
@@ -1,7 +1,43 @@
+import { Button } from "@/components/ui/button";
+
+const ITEMS = [
+  {
+    title: "Most Popular",
+    key: "#",
+  },
+  {
+    title: "Most Sold",
+    key: "#",
+  },
+  {
+    title: "Most Expensive",
+    key: "#",
+  },
+  {
+    title: "Most Cheap",
+    key: "#",
+  },
+];
+
 const Filters = () => {
   return (
-    <div className="p-3 w-80 border rounded-lg h-80">
-      <p>Filters</p>
+    <div className="p-3 w-80 border rounded-lg h-80 flex flex-col items-start">
+      <div className="flex justify-between w-full items-center">
+        <p className="font-semibold text-primary/90">Filters</p>
+        {/* <Button variant="ghost" className="text-[#19bfd3] text-xs font-bold hover:text-[#19bfd3]/80 bg-">
+          Clear All
+        </Button> */}
+      </div>
+      <div className="flex flex-col items-start gap-2 mt-2 w-full">
+        {ITEMS.map((item, index) => (
+          <div
+            key={index}
+            className="border p-2 w-full text-left text-sm font-semibold text-muted-foreground rounded-md hover:bg-muted cursor-pointer"
+          >
+            <p>{item.title}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
