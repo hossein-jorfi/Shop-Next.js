41ef38c0e564828654dde204a38d65b1848e72db
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-20 22:33:01 +0330

    handleClear

--- src/ui/shop/sorts.tsx
+++ src/ui/shop/sorts.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
 
 const ITEMS = [
   {
@@ -25,6 +25,7 @@ const ITEMS = [
 ];
 
 const Sorts = () => {
+  const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const categories = searchParams.get("categories");
@@ -37,6 +38,14 @@ const Sorts = () => {
     return newHref;
   };
 
+  const handleClear = () => {
+    const newHref = `${pathname}${
+      categories ? `?categories=${categories}` : ""
+    }`;
+
+    router.push(newHref);
+  };
+
   return (
     <div className="p-3 w-80 border rounded-lg h-80 flex flex-col items-start">
       <div className="flex justify-between w-full items-center">
@@ -45,6 +54,7 @@ const Sorts = () => {
           variant="ghost"
           className="text-custom-blue font-bold hover:text-custom-blue hover:bg-custom-blue/10"
           size="sm"
+          onClick={handleClear}
         >
           Clear
         </Button>
@@ -54,7 +64,6 @@ const Sorts = () => {
           <Link
             key={index}
             href={generateHref(item.key)}
-            // className="border p-2 w-full text-left text-sm font-semibold text-muted-foreground rounded-md hover:bg-muted cursor-pointer hover:text-primary hover:border-primary"
             className="w-full text-left text-sm font-semibold"
           >
             <Button
