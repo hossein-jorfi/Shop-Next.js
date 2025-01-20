487a1be72342980fd004af7ac119348cc576403c
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-20 22:21:03 +0330

    handle url params for sort

--- src/ui/shop/sorts.tsx
+++ src/ui/shop/sorts.tsx
@@ -1,25 +1,41 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
 
 const ITEMS = [
   {
     title: "Most Popular",
-    key: "#",
+    key: "popular",
   },
   {
     title: "Most Sold",
-    key: "#",
+    key: "sold",
   },
   {
     title: "Most Expensive",
-    key: "#",
+    key: "expensive",
   },
   {
     title: "Most Cheap",
-    key: "#",
+    key: "cheap",
   },
 ];
 
 const Sorts = () => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const categories = searchParams.get("categories");
+  // const sort = searchParams.get("sort");
+
+  const generateHref = (key: string) => {
+    const newHref = `${pathname}?sort=${key}${
+      categories ? `&categories=${categories}` : ""
+    }`;
+    return newHref;
+  };
+
   return (
     <div className="p-3 w-80 border rounded-lg h-80 flex flex-col items-start">
       <div className="flex justify-between w-full items-center">
@@ -34,12 +50,13 @@ const Sorts = () => {
       </div>
       <div className="flex flex-col items-start gap-2 mt-2 w-full">
         {ITEMS.map((item, index) => (
-          <div
+          <Link
+            href={generateHref(item.key)}
             key={index}
             className="border p-2 w-full text-left text-sm font-semibold text-muted-foreground rounded-md hover:bg-muted cursor-pointer"
           >
             <p>{item.title}</p>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
