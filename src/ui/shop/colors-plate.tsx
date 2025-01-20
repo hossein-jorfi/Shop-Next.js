a80b920c18bf2060b1d1c627e539a2d33d45a5dd
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-20 10:11:19 +0330

    ColorsPlate random

--- src/ui/shop/colors-plate.tsx
+++ src/ui/shop/colors-plate.tsx
@@ -1,5 +1,22 @@
+const ITEMS = [
+  "bg-sky-400",
+  "bg-orange-400",
+  "bg-gray-400",
+  "bg-rose-400",
+  "bg-lime-400",
+];
+
 const ColorsPlate = () => {
-  return <div className="absolute">ColorsPlate</div>;
+  return (
+    <div className="absolute top-2 flex pl-3">
+      {ITEMS.map((item, index) => (
+        <div
+          key={index}
+          className={`w-4 h-4 -ml-1.5 border border-white rounded-full ${item}`}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default ColorsPlate;
