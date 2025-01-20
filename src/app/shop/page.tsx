86d4222b9eb760c30251e4cd9708d6949e2fb0be
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-20 07:50:31 +0330

    Products components

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,14 +1,19 @@
+import { ProductType } from "@/definitions";
 import Filters from "@/ui/shop/filters";
+import Products from "@/ui/shop/products";
 import ShopBreadcrumb from "@/ui/shop/shop-breadcrumb";
 
-const Shop = () => {
+const Shop = async () => {
+  const data = await fetch(`${process.env.BASE_URl}/products`);
+  const products: ProductType[] | undefined = await data.json();
+
   return (
     <div className="flex flex-col items-start custom-container content-paddign-x content-paddign-y h-full">
       <ShopBreadcrumb />
 
       <div className="mt-5 flex gap-3 w-full h-full">
         <Filters />
-        <div className="p-3 w-full border h-full">Products</div>
+        <Products />
       </div>
     </div>
   );
---
+++ src/ui/shop/products.tsx
@@ -0,0 +1,9 @@
+const Products = () => {
+  return (
+    <div className="p-3 w-full border h-full">
+      <p>Products</p>
+    </div>
+  );
+};
+
+export default Products;
