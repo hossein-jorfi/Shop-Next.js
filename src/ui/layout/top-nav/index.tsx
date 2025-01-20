e3773c4010d38a2f66212624a3316e6a52c887fc
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-20 21:53:07 +0330

    wrap nav items in suspence

--- src/ui/layout/top-nav/index.tsx
+++ src/ui/layout/top-nav/index.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import BottomSection from "./bottom-section";
 import TopSection from "./top-section";
 
@@ -6,7 +7,10 @@ const TopNav = () => {
     <div className="border-b shadow content-paddign-x content-paddign-y sm:!pb-0 fixed top-0 w-full bg-background z-10">
       <div className="custom-container">
         <TopSection />
-        <BottomSection />
+
+        <Suspense>
+          <BottomSection />
+        </Suspense>
       </div>
     </div>
   );
