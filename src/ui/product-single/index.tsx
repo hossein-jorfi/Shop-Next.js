050ea2079c3b69b26c9ed000154688a36f0b3c8b
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-21 18:13:49 +0330

    product data

--- src/ui/product-single/index.tsx
+++ src/ui/product-single/index.tsx
@@ -1,7 +1,17 @@
 import { ProductType } from "@/definitions";
 
 const ProductSingle = ({ product }: { product: ProductType | undefined }) => {
-  return <div>{product?.description}</div>;
+  return (
+    <div className="pt-5 border custom-container">
+      <p>{product?.title}</p>
+      <p>{product?.description}</p>
+      <p>{product?.category}</p>
+      <p>{product?.image}</p>
+      <p>{product?.price}</p>
+      <p>{product?.rating.rate}</p>
+      <p>{product?.rating.count}</p>
+    </div>
+  );
 };
 
 export default ProductSingle;
