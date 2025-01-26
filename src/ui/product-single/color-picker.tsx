4b19f27025a61eca8b5aca96cf7a59a2bad25290
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-01-26 21:07:17 +0330

    color picker change item

--- src/ui/product-single/color-picker.tsx
+++ src/ui/product-single/color-picker.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
+import { useState } from "react";
 
 const ITEMS = [
   {
@@ -30,13 +31,24 @@ const ITEMS = [
 ];
 
 const ColorPicker = () => {
+  const [selected, setSelected] = useState(ITEMS[0].title);
+
   return (
     <div className="flex gap-2">
       {ITEMS.map((item, index) => (
         <div
           key={index}
-          className={cn(item.className, "w-7 h-7 rounded-full")}
-        />
+          onClick={() => setSelected(item.title)}
+          className={cn(
+            "rounded-full p-0.5 cursor-pointer",
+            selected === item.title &&
+              "outline outline-2 outline-blue-400"
+          )}
+        >
+          <div
+            className={cn(item.className, "w-7 h-7 rounded-full p-3")}
+          />
+        </div>
       ))}
     </div>
   );
