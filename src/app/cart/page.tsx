643c94d6bc9454fa75c74ffc41240134b639cb4b
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-02-01 15:36:05 +0330

    cart component & route

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,5 +1,9 @@
 export const Page = () => {
-  return <div>Cart</div>;
+  return (
+    <div className="pt-5 custom-container content-paddign-x">
+      <p>Cart</p>
+    </div>
+  );
 };
 
 export default Page;
