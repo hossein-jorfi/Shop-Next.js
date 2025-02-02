533167b27f9350fd1934526849e43518924d5038
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-02-02 23:26:17 +0330

    products style

--- src/ui/shop/product-card.tsx
+++ src/ui/shop/product-card.tsx
@@ -3,7 +3,7 @@ import { Star } from "lucide-react";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { ProductType } from "@/definitions";
 import ColorsPlate from "./colors-plate";
-import ClockImage from '@/assets/shop/clock.png'
+import ClockImage from "@/assets/shop/clock.png";
 import Link from "next/link";
 
 type Props = {
@@ -12,17 +12,24 @@ type Props = {
 
 const ProductCard = ({ product }: Props) => {
   return (
-    <Link href={`/product/${product.id}`} className="flex flex-col justify-between items-start select-none cursor-pointer relative border p-4 hover:shadow-same">
-      <AspectRatio className="flex justify-center items-center">
-        <Image
-          src={product.image}
-          alt="product"
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: "50%", height: "auto" }}
-        />
-      </AspectRatio>
+    <Link
+      href={`/product/${product.id}`}
+      className="flex flex-col justify-between items-start select-none cursor-pointer relative border p-4 hover:shadow-same"
+    >
+      <div className="w-full flex justify-center items-center">
+        <div className="w-44 h-44 flex justify-center items-center">
+          <AspectRatio className="flex justify-center items-center">
+            <Image
+              src={product.image}
+              alt="product"
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="w-1/2 h-auto"
+            />
+          </AspectRatio>
+        </div>
+      </div>
 
       <p className="h-[44px] text-sm font-semibold text-primary/90 overflow-hidden">
         {product.title}
@@ -45,11 +52,7 @@ const ProductCard = ({ product }: Props) => {
 
         <div className="flex items-center text-xs font-medium text-muted-foreground gap-1">
           <p>Special Delivery</p>
-          <Image
-            src={ClockImage}
-            alt="Special Delivery"
-            className="w-5"
-          />
+          <Image src={ClockImage} alt="Special Delivery" className="w-5" />
         </div>
       </div>
 
