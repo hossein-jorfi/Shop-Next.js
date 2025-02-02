f1ace323e76abb10467cec652df5faabb9ea1a36
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-02-02 21:43:16 +0330

    final buy loading

--- src/ui/cart/cart-loading.tsx
+++ src/ui/cart/cart-loading.tsx
@@ -1,8 +1,15 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 const CartLoading = () => {
   return (
     <>
       <div className="w-[300px] shrink-0">
-        <div className="border rounded-lg p-3 flex flex-col gap-2"></div>
+        <div className="border rounded-lg p-3 flex flex-col gap-2">
+          <InfoItem />
+          <InfoItem />
+          <InfoItem />
+          <Skeleton className="h-9" />
+        </div>
       </div>
       <div className="flex-grow">
         <div className="flex flex-col gap-2 border"></div>
@@ -12,3 +19,10 @@ const CartLoading = () => {
 };
 
 export default CartLoading;
+
+const InfoItem = () => {
+  return <div className="flex justify-between items-center gap-2">
+      <Skeleton className="w-2/3 h-5" />
+      <Skeleton className="w-1/3 h-5" />
+  </div>;
+};
