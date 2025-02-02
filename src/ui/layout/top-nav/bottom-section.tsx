505d3fd32e50b96dfe2e0417ba837cf7649f6873
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-02-02 22:55:22 +0330

    Categories menu for mobile

--- src/ui/layout/top-nav/bottom-section.tsx
+++ src/ui/layout/top-nav/bottom-section.tsx
@@ -3,6 +3,8 @@
 import Link from "next/link";
 import { usePathname, useSearchParams } from "next/navigation";
 import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
 
 const ITEMS = [
   {
@@ -38,33 +40,41 @@ const BottomSection = () => {
   const categories = searchParams.get("categories");
 
   return (
-    <div className="mt-4 hidden sm:flex font-semibold text-sm pb-2 space-x-4">
-      {ITEMS.map((item, index) => (
-        <Link
-          key={index}
-          href={item.href}
-          className="relative group cursor-pointer"
-        >
-          <p
-            className={cn(
-              "text-muted-foreground hover:text-primary transition-all",
-              categories?.toLowerCase() === item?.key?.toLowerCase() &&
-                pathname === "/shop" &&
-                "text-primary"
-            )}
+    <div className="mt-4 font-semibold text-sm pb-0 sm:pb-2">
+      <div className="hidden sm:flex gap-4">
+        {ITEMS.map((item, index) => (
+          <Link
+            key={index}
+            href={item.href}
+            className="relative group cursor-pointer"
           >
-            {item.title}
-          </p>
-          <div
-            className={cn(
-              "h-[2px] rounded absolute -bottom-2 w-0 group-hover:!w-full bg-red transition-all ease-out duration-300",
-              categories?.toLowerCase() === item?.key?.toLowerCase() &&
-                pathname === "/shop" &&
-                "w-full"
-            )}
-          />
-        </Link>
-      ))}
+            <p
+              className={cn(
+                "text-muted-foreground hover:text-primary transition-all",
+                categories?.toLowerCase() === item?.key?.toLowerCase() &&
+                  pathname === "/shop" &&
+                  "text-primary"
+              )}
+            >
+              {item.title}
+            </p>
+            <div
+              className={cn(
+                "h-[2px] rounded absolute -bottom-2 w-0 group-hover:!w-full bg-red transition-all ease-out duration-300",
+                categories?.toLowerCase() === item?.key?.toLowerCase() &&
+                  pathname === "/shop" &&
+                  "w-full"
+              )}
+            />
+          </Link>
+        ))}
+      </div>
+      <div className="flex sm:hidden">
+        <Button variant='ghost' className="p-0 m-0 w-fit h-fit">
+          <Menu />
+          Categories
+        </Button>
+      </div>
     </div>
   );
 };
