23ce488fb7b83f2d883c8f0e847e0c29e5c05b74
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-02-02 11:36:01 +0330

    ProductInfo

--- src/ui/cart/products/product-info.tsx
+++ src/ui/cart/products/product-info.tsx
@@ -1,7 +1,31 @@
+import { ReactNode } from "react";
+
 export const ProductInfo = () => {
-    return (
-        <div>
-            ProductInfo
-        </div>
-    );
-}
+  return (
+    <div className="flex flex-col gap-2">
+      <p className="font-semibold text-primary/90">
+        Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+      </p>
+      <div className="flex flex-col">
+        <InfoItem text="black">
+          <div className="w-4 h-4 rounded-full bg-black" />
+        </InfoItem>
+      </div>
+    </div>
+  );
+};
+
+const InfoItem = ({
+  children,
+  text,
+}: {
+  text: string;
+  children: ReactNode;
+}) => {
+  return (
+    <div className="flex items-center gap-1.5">
+      <div>{children}</div>
+      <p className="text-muted-foreground text-sm font-medium">{text}</p>
+    </div>
+  );
+};
