09dfbf1afc31baef722c2ba9e2b929519d9a26b8
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-02-02 22:40:02 +0330

    ProductItem handleNavigate

--- src/ui/cart/products/product-item.tsx
+++ src/ui/cart/products/product-item.tsx
@@ -3,12 +3,19 @@ import Image from "next/image";
 import { ManageButton } from "./manage-button";
 import { ProductInfo } from "./product-info";
 import { ProductType } from "@/definitions";
+import { useRouter } from "next/navigation";
 
 interface Props {
   product: ProductType | undefined;
 }
 
 export const ProductItem = ({ product }: Props) => {
+  const router = useRouter();
+
+  const handleNavigate = () => {
+    router.push(`/product/${product?.id}`);
+  };
+
   const renderImage = (
     <div className="h-28 w-28 cursor-pointer">
       <AspectRatio className="flex justify-center items-center">
@@ -24,9 +31,16 @@ export const ProductItem = ({ product }: Props) => {
 
   return (
     <div className="flex flex-col md:flex-row gap-2 p-3">
-      <div className="flex md:hidden border rounded-lg justify-center items-center">{renderImage}</div>
+      <div
+        className="flex md:hidden border rounded-lg justify-center items-center"
+        onClick={handleNavigate}
+      >
+        {renderImage}
+      </div>
       <div className="flex flex-col items-center justify-between">
-        <div className="hidden md:block">{renderImage}</div>
+        <div className="hidden md:block" onClick={handleNavigate}>
+          {renderImage}
+        </div>
         <div className="hidden md:block">
           <ManageButton id={product?.id || 0} />
         </div>
