892a8d79a03cdda927e7e4e9f96f50f61f36c190
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-02-02 08:58:20 +0330

    ManageButton

---
+++ src/ui/cart/products/manage-button.tsx
@@ -0,0 +1,7 @@
+export const ManageButton = () => {
+    return (
+        <div>
+            ManageButton
+        </div>
+    );
+}
--- src/ui/cart/products/product-item.tsx
+++ src/ui/cart/products/product-item.tsx
@@ -1,6 +1,26 @@
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import Image from "next/image";
+import { ManageButton } from "./manage-button";
+
 export const ProductItem = () => {
   return (
-    <div className="border h-32"></div>
+    <div className="border flex p-3">
+      <div className="flex flex-col items-center">
+        <div className="h-28 w-28">
+          <AspectRatio className="flex justify-start items-center">
+            <Image
+              src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+              alt="product"
+              width={60}
+              height={60}
+            />
+          </AspectRatio>
+        </div>
+        <ManageButton />
+      </div>
+
+      <p>test</p>
+    </div>
   );
 };
 
