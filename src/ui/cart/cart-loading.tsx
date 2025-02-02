fe1fab99fce5f3a4d7540e77dfcc4dc4cecd134f
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-02-02 22:11:37 +0330

    CartLoading

--- src/ui/cart/cart-loading.tsx
+++ src/ui/cart/cart-loading.tsx
@@ -1,3 +1,4 @@
+import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
 
 const CartLoading = () => {
@@ -12,7 +13,16 @@ const CartLoading = () => {
         </div>
       </div>
       <div className="flex-grow">
-        <div className="flex flex-col gap-2 border"></div>
+        <div className="flex flex-col gap-2">
+          {Array(5)
+            .fill(0)
+            .map((_, index) => (
+              <>
+                <ProductItem key={index} />
+                <Separator className="bg-border" />
+              </>
+            ))}
+        </div>
       </div>
     </>
   );
@@ -20,9 +30,32 @@ const CartLoading = () => {
 
 export default CartLoading;
 
+const ProductItem = () => {
+  const Info = <Skeleton className="w-full h-5" />;
+  return (
+    <div className="flex gap-2 p-3">
+      <div className="flex flex-col items-center justify-between">
+        <Skeleton className="w-28 h-28" />
+        <Skeleton className="w-full h-8" />
+      </div>
+
+      <div className="flex flex-col gap-2 w-full">
+        {Info}
+        {Info}
+        {Info}
+        {Info}
+        {Info}
+        {Info}
+      </div>
+    </div>
+  );
+};
+
 const InfoItem = () => {
-  return <div className="flex justify-between items-center gap-2">
+  return (
+    <div className="flex justify-between items-center gap-2">
       <Skeleton className="w-2/3 h-5" />
       <Skeleton className="w-1/3 h-5" />
-  </div>;
+    </div>
+  );
 };
