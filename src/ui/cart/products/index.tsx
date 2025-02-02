b3d1f15286006e7260837a709fd8d9011f4493a8
hosseinjorfi <hosseinjorfi81@gmail.com>
2025-02-02 13:36:44 +0330

    show cart Products

--- src/ui/cart/products/index.tsx
+++ src/ui/cart/products/index.tsx
@@ -2,15 +2,24 @@ import { Separator } from "@/components/ui/separator";
 import ProductItem from "./product-item";
 import { ProductType } from "@/definitions";
 import React from "react";
+import useCartStore from "@/store/useCartStore";
 
 interface Props {
   products: ProductType[] | undefined;
 }
 
 export const Products = ({ products }: Props) => {
+  const cartProductsIds = useCartStore((state) => state.products).map(
+    (i) => i.id
+  );
+
+  const filtredProducts = products?.filter((item) =>
+    cartProductsIds?.includes(item.id)
+  );
+
   return (
     <div className="flex flex-col gap-2">
-      {products?.map((product) => (
+      {filtredProducts?.map((product) => (
         <React.Fragment key={product.id}>
           <ProductItem product={product} />
           <Separator className="bg-border" />
